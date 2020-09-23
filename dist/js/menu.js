91672d8a012d746c6e6631b7eb0c9da9f6e0840e
duongductrong <duongductrong06@gmail.com>
2020-09-23 15:02:28 +0700

    add overlay for menu

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -1,17 +1,37 @@
 window.addEventListener("DOMContentLoaded", () => {
-    let activeListItem = false;
-    let menuList = document.querySelector(".menu-list");
-  
-    document.querySelectorAll(".menu-bars").forEach((bar) => {
-      bar.addEventListener("click", () => {
-        activeListItem = !activeListItem;
-  
-        if (activeListItem) {
-          menuList.classList.add(`menu-active`);
-        } else {
-          menuList.classList.remove("menu-active");
-        }
-      });
+  let activeListItem = false;
+  let menuList = document.querySelector(".menu-list");
+  let divOverlay = document.createElement("div");
+
+  divOverlay.setAttribute("class", "overlay");
+  window.document.body.append(divOverlay);
+
+  document.querySelectorAll(".menu-bars").forEach((bar) => {
+    bar.addEventListener("click", () => {
+      activeListItem = !activeListItem;
+
+      if (activeListItem) {
+        menuList.classList.add(`menu-active`);
+
+        document.querySelector(".menu").style.background =
+          "#ffffff url('/dist/images/core-bg.svg')";
+
+        document.querySelector(".overlay").classList.add("overlay-active");
+        document
+          .querySelector(".overlay")
+          .classList.remove("overlay-hidden");
+      } else {
+        menuList.classList.remove("menu-active");
+
+        document.querySelector(".menu").style.background = "white";
+
+        // window.document.body.removeChild(divOverlay);
+
+        document
+          .querySelector(".overlay")
+          .classList.remove("overlay-active");
+        document.querySelector(".overlay").classList.add("overlay-hidden");
+      }
     });
   });
-  
+});
