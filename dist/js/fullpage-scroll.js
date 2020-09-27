b5a5be2a7fca3a03c149564d0f49a3193d04e152
Nguyen Tuan <nguyenquoctuan832@gmail.com>
2020-09-27 19:03:50 +0700

    responsive for htdt

--- dist/js/fullpage-scroll.js
+++ dist/js/fullpage-scroll.js
@@ -5,20 +5,22 @@ function FullpageScroll(i) {
         totalHeight += pages[index].offsetHeight;
     }
 
-    window.y = window.pageYOffset;
+    window.y = document.body.scrollTop;
     window.scrollHeight = totalHeight;
-
+    document.body.scroll(0, totalHeight);
+    return;
     if (window.scrolling) return;
     window.scrollSpeed = 0.1;
 
     function scroll() {
+        console.log(document.body.scrollTop);
         window.scrolling = true;
-        window.scroll(0, window.y);
+        document.body.scroll(0, window.y);
         window.y += (window.scrollHeight - window.y) * window.scrollSpeed;
         window.scrollAnimation = window.requestAnimationFrame(scroll);
         if (Math.abs(window.scrollHeight - window.y) <= 1) {
             window.scrolling = false;
-            window.scroll(0, window.scrollHeight);
+            document.body.scroll(0, window.scrollHeight);
             window.cancelAnimationFrame(window.scrollAnimation);
         }
         return;
